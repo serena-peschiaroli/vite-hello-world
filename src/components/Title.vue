31fd3f08ce621425f11a320b0beaf04952622cf2
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-11-22 17:05:15 +0100

    Add: project init

---
+++ src/components/Title.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    data(){
+        return {
+            greetings: "La mia prima app con Vite!"
+        }
+    }
+}
+</script>
+
+<template>
+    <h2> {{ greetings }} </h2>
+
+</template>
+
+<style>
+</style>
