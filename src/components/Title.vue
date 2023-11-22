93588d02177fba6b78d747544af3dedc031fe811
serena-peschiaroli <serena.peschiaroli@gmail.com>
2023-11-22 17:21:40 +0100

    Add: first component Title; second component button, methods for showing paragraph

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -2,14 +2,25 @@
 export default {
     data(){
         return {
-            greetings: "La mia prima app con Vite!"
-        }
-    }
-}
+            greetings: "La mia prima app con Vite!",
+            mostraParagrafo: false,
+        };
+    },
+    methods: {
+        message() {
+            this.mostraParagrafo = true;
+        },
+    },
+};
 </script>
 
 <template>
-    <h2> {{ greetings }} </h2>
+    <div>
+        <h2> {{ greetings }} </h2>
+        <button @click="message"> cliccami! </button>
+        <p v-if="mostraParagrafo"> il mio primo bottone con Vite! </p>
+    </div>
+    
 
 </template>
 
